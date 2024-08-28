7de35f8ef7643bbae2afc159717e95ea70051adf
John Doe <manuel-torres34@hotmail.com>
2024-08-28 08:48:55 -0500

    first commit

---
+++ principal.py
@@ -0,0 +1,8 @@
+from suma import funcion_suma
+from modulo_producto import funcion_producto
+
+print("calculadora")
+valor_1=int(input("Ingresa el primer valor"))
+valor_2=int(input("Ingresa el segundo valor"))
+print("El resultado es: ", funcion_suma(valor_1,valor_2))
+print("El resultado es: ", funcion_producto(valor_1,valor_2))
---
+++ suma.py
@@ -0,0 +1,6 @@
+def suma(valor1, valor2):
+
+    return valor1 + valor2
+def funcion2():
+    
+    return("Mensaje de prueba")
